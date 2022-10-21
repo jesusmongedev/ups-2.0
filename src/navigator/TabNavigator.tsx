dbd15024597b0fd04d32b9d1b703f0085b570ad0
Jesús Andrés Monge <75322982+jesusmongedev@users.noreply.github.com>
2022-10-20 23:52:10 -0500

    {Feat} Setting Up React Native Navigation

---
+++ src/navigator/TabNavigator.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import CustomersScreen from "../screens/CustomersScreen/index";
+import OrdersScreen from "../screens/OrdersScreen";
+import { Colors } from "../shared/theme/Colors";
+import { Icon } from "@rneui/themed";
+
+export type TabParamList = {
+  Customers: undefined;
+  Orders: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabParamList>();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route: { name } }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: Colors.primary,
+        tabBarInactiveTintColor: Colors.gray,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName = "home";
+          let iconColor = color;
+
+          if (name === "Customers") {
+            iconName = "users";
+          } else if (name === "Orders") {
+            iconName = "box";
+            iconColor = focused ? Colors.secondary : Colors.gray;
+          }
+
+          return (
+            <Icon name={iconName} type='entypo' size={size} color={iconColor} />
+          );
+        },
+      })}>
+      <Tab.Screen name='Customers' component={CustomersScreen} />
+      <Tab.Screen name='Orders' component={OrdersScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigator;
