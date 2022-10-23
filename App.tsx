0490282f8252d21dbf053a873398cd0e1dbccdae
Jesús Andrés Monge <75322982+jesusmongedev@users.noreply.github.com>
2022-10-22 20:25:25 -0500

    [Update] setting up apollo client Add apollo client and create queries to fetch data

--- App.tsx
+++ App.tsx
@@ -1,16 +1,24 @@
 import { TailwindProvider } from "tailwind-rn";
 import { NavigationContainer } from "@react-navigation/native";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import utilities from "./tailwind.json";
 import RootNavigator from "./src/navigator/RootNavigator";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5001/api/ups-graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
